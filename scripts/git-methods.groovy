bfda02316e746841e6911edb1dce34ffe82bf2c6
Dishant Anand <d.synchronized@gmail.com>
2021-09-08 16:57:15 +0530

    committed jenkins file

--- scripts/git-methods.groovy
+++ scripts/git-methods.groovy
@@ -1,8 +1,8 @@
 def checkourSourceCode(String branchName, String credentialsId, String repositoryUrl) {
    checkout([$class: 'GitSCM', 
-               branches: [[name: branchName]], 
+               branches: [[name: "${branchName}"]], 
                extensions: [], 
-               userRemoteConfigs: [[credentialsId: credentialsId, url: repositoryUrl]]])
+               userRemoteConfigs: [[credentialsId: "${credentialsId}", url: "${repositoryUrl}"]]])
 }
 
 return this;
