46f4c4e7b1dcc61ebcf071de148c01d20c36aaad
Dishant Anand <d.synchronized@gmail.com>
2021-09-08 17:33:56 +0530

    committed jenkins file

--- scripts/gitMethods.groovy
+++ scripts/gitMethods.groovy
@@ -1,9 +1,5 @@
 def checkourSourceCode(String branchName, String credentialsId, String repositoryUrl) {
    echo 'inside the checkout source code method'
-   checkout([$class: 'GitSCM', 
-               branches: [[name: "${branchName}"]], 
-               extensions: [], 
-               userRemoteConfigs: [[credentialsId: "${credentialsId}", url: "${repositoryUrl}"]]])
 }
 
 return this;
