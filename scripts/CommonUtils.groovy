23d1569eef819067b35f1a806a3b666e8a1ed416
Dishant Anand <d.synchronized@gmail.com>
2021-09-15 18:36:55 +0530

    committing code files

--- scripts/CommonUtils.groovy
+++ scripts/CommonUtils.groovy
@@ -22,12 +22,9 @@ def downloadArtifacts(String pattern, String target){
   def buildInfo
   try{
    buildInfo = server.download spec: downloadSpec, failNoOp: true
+   server.publishBuildInfo buildInfo
   }catch(err){
-    echo "${err}"
     buildInfo = null
-  }
-  if(buildInfo != null){
-    server.publishBuildInfo buildInfo
   }
   return buildInfo                 
 }
